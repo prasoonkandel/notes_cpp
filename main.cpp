a5b7496bd0d4976c47a360b35b3e4ad7562da842
Prasoon Kandel <prasoonkandel1029@gmail.com>
2026-04-05 22:23:53 +0545

    feat: made a basic structure for prototype

--- main.cpp
+++ main.cpp
@@ -1,9 +1,57 @@
 #include <iostream>
-
+#include <fstream>
+#include <string>
 using namespace std;
 
 int main() {
+    bool appRunning = true;
+    cout<<"\033[36m";
+    cout<<R"(
+███╗   ██╗ ██████╗ ████████╗███████╗███████╗     ██████╗██████╗ ██████╗ 
+████╗  ██║██╔═══██╗╚══██╔══╝██╔════╝██╔════╝    ██╔════╝██╔══██╗██╔══██╗
+██╔██╗ ██║██║   ██║   ██║   █████╗  ███████╗    ██║     ██████╔╝██████╔╝
+██║╚██╗██║██║   ██║   ██║   ██╔══╝  ╚════██║    ██║     ██╔═══╝ ██╔═══╝ 
+██║ ╚████║╚██████╔╝   ██║   ███████╗███████║    ╚██████╗██║     ██║     
+╚═╝  ╚═══╝ ╚═════╝    ╚═╝   ╚══════╝╚══════╝     ╚═════╝╚═╝     ╚═╝                                                                          
+)";
+    cout<<"\033[0m";
 
-cout<<"Hello, World!";	
+    while(appRunning){
+        int choice = 0;
 
+        cout<<"\n============================\n";
+        cout<<"1. Create New File\n";
+        cout<<"2. Read File\n";
+        cout<<"3. Write File\n";
+        cout<<"4. List Files\n";
+        cout<<"5. Exit \n";
+        cout<<"============================\n";
+        cout<<"Enter your choice: ";
+        cin >> choice;
+        cin.ignore();
+
+        string filename, text;
+
+        switch(choice){
+        case 1: 
+            cout<<"Enter file name: ";
+  			break;
+
+        case 2: 
+            cout<<"Enter file name: ";
+        	break;
+
+        case 3: 
+            cout<<"Enter file name: ";
+       		break;
+
+        case 4:
+            cout<<"Bye!\n";
+            appRunning = false;
+            break;
+
+        default:
+            cout<<"Invalid option\n";
+        }}
+    return 0;
 }
